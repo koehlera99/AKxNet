a50fb8fbe579962caf56396acd4f581c5af12074
Aaron Koehler <akxnet@gmail.com>
2017-10-04 19:40:55 -0500

    Base

---
+++ DnDWeb/DnDWeb/Models/DndObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnDWeb.Models
+{
+    public abstract class DndObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public DndObject() { }
+        public DndObject(int id)
+        {
+            id = Id;
+        }
+
+        public DndObject(string name)
+        {
+            Name = name;
+        }
+
+        public DndObject(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public DndObject(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}
