9c6fd6c5b08e37d09770b756f63bf1de0826779c
Aaron Koehler <akxnet@gmail.com>
2018-04-05 15:59:03 -0500

    new

---
+++ Prime/UrhoSharp.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UrhoSharp.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var app = await UrhoSurface.Show<BattleGrid>(new Urho.ApplicationOptions("MyData"));
+        }
+    }
+}
